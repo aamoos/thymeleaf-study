a1c01dbbb2190de7bd41d7d39a4e30185483b4b5
= <aamoos@naver.com>
2022-07-08 18:22:47 +0900

    타임리프 공부

--- src/main/java/com/example/thymeleafstudy/controller/MainController.java
+++ src/main/java/com/example/thymeleafstudy/controller/MainController.java
@@ -1,8 +1,12 @@
 package com.example.thymeleafstudy.controller;
 
+import org.jetbrains.annotations.NotNull;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.time.LocalDate;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -10,7 +14,7 @@ import java.util.Map;
 public class MainController {
 
     @GetMapping("/")
-    public String main(Model model){
+    public String main(@NotNull Model model){
         Map<String, Object> foodMap = new HashMap<String, Object>();
         foodMap.put("food1", "치킨");
         foodMap.put("food2", "<strong>피자</strong>");
@@ -18,9 +22,24 @@ public class MainController {
         foodMap.put("food4", "파스타");
 
         model.addAttribute("foodMap", foodMap);
+        model.addAttribute("localDateTime", LocalDate.now());
 
         return "index.html";
     }
+
+    @GetMapping("/date")
+    public String date(@NotNull Model model){
+        model.addAttribute("localDateTime", LocalDate.now());
+        return "date.html";
+    }
+
+    @Component("helloBean")
+    static class HelloBean{
+        public String hello(String data){
+            return "Hello" +data;
+        }
+    }
+
 
 
 }
