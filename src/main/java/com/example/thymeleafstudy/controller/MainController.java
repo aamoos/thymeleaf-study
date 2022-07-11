861557c60514f933fc5cc162f74d1251ea791bcd
= <aamoos@naver.com>
2022-07-11 09:48:06 +0900

    타임리프 공부

--- src/main/java/com/example/thymeleafstudy/controller/MainController.java
+++ src/main/java/com/example/thymeleafstudy/controller/MainController.java
@@ -7,19 +7,32 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Controller
 public class MainController {
 
     @GetMapping("/")
-    public String main(@NotNull Model model){
+    public static String main(@NotNull Model model){
         Map<String, Object> foodMap = new HashMap<String, Object>();
         foodMap.put("food1", "치킨");
         foodMap.put("food2", "<strong>피자</strong>");
         foodMap.put("food3", "삼겹살");
         foodMap.put("food4", "파스타");
+
+        List<Map<String, Object>> list = new ArrayList<Map<String, Object>>();
+
+        for(int i=0; i<10; i++){
+            Map<String, Object> listMap = new HashMap<String, Object>();
+            listMap.put("userName", "userName"+i);
+            listMap.put("userId", "userId"+i);
+            list.add(listMap);
+        }
+
+        model.addAttribute("list", list);
 
         model.addAttribute("foodMap", foodMap);
         model.addAttribute("localDateTime", LocalDate.now());
